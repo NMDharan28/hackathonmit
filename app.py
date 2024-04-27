42c0360c29aa93097ab2878d5e59c3c5aae3c7eb
NMDharan28 <nmdharan28@gmail.com>
2024-04-27 20:26:59 +0530

    Cleared by NK

--- app.py
+++ app.py
@@ -13,31 +13,16 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    energy_type = request.form['energy_type']
-    if energy_type == 'solar':
-        return redirect(url_for('predict_solar'))
-    elif energy_type == 'wind':
-        return redirect(url_for('predict_wind'))
-
-@app.route('/predict/solar')
-def predict_solar():
+@app.route('/solar',methods=['GET'])
+def solarpredict():
     return render_template('solar.html')
 
-@app.route('/predict/wind')
-def predict_wind():
+@app.route('/wind',methods=['GET'])
+def windpredict():
     return render_template('wind.html')
 
-@app.route('/predict/solar/result', methods=['POST'])
-def predict_solar_result():
-    # Get input data from the form
-    # Assume similar input fields as in the previous code for solar prediction
-    # ...
-
-    # Make prediction using the solar model
-    # ...
-
+@app.route('/solar/predict', methods=['POST'])
+def predict1():
     cloud_coverage = float(request.form['cloud_coverage'])
     visibility = float(request.form['visibility'])
     temperature = float(request.form['temperature'])
@@ -45,35 +30,91 @@ def predict_solar_result():
     relative_humidity = float(request.form['relative_humidity'])
     wind_speed = float(request.form['wind_speed'])
     station_pressure = float(request.form['station_pressure'])
+    prediction=solarresult(cloud_coverage,visibility,temperature, dew_point, relative_humidity, wind_speed, station_pressure)
+    return render_template('solar_result.html',prediction= prediction)
+
+@app.route('/wind/predict', methods=['POST'])
+def predict2():
+    pressure = float(request.form['pressure'])
+    wind_direction = float(request.form['wind_direction'])
+    wind_speed = float(request.form['wind_speed'])
+    prediction = windresult(pressure, wind_direction, wind_speed)
+    return render_template('wind_result.html',prediction=prediction)
+
+
+
+
+#@app.route('/predict', methods=['POST'])
+#def predict():
+ #   energy_type = request.form['energy_type']
+  #  if energy_type == 'solar':
+#   return redirect(url_for('predict_solar'))
+ #   elif energy_type == 'wind':
+  #      return redirect(url_for('predict_wind')
+
+
+#@app.route('/predict/solar/result', methods=['POST'])
+#def predict_solar_result():
+    # Get input data from the form
+    # Assume similar input fields as in the previous code for solar prediction
+    # ...
+
+    # Make prediction using the solar model
+    # ...
+
+    #cloud_coverage = float(request.form['cloud_coverage'])
+    #visibility = float(request.form['visibility'])
+    #temperature = float(request.form['temperature'])
+    #dew_point = float(request.form['dew_point'])
+    #relative_humidity = float(request.form['relative_humidity'])
+    #wind_speed = float(request.form['wind_speed'])
+    #station_pressure = float(request.form['station_pressure'])
     
     # Impute missing values
+    #imputer = SimpleImputer(strategy='mean')
+    #X_input = [[cloud_coverage, visibility, temperature, dew_point, relative_humidity, wind_speed, station_pressure]]
+    #X_input_imputed = imputer.fit_transform(X_input)
+
+    # Make prediction
+    #prediction = solar_model.predict(X_input_imputed)[0]
+
+    #return render_template('solar_result.html', prediction=prediction)
+def solarresult(cloud_coverage,visibility,temperature, dew_point, relative_humidity, wind_speed, station_pressure):
     imputer = SimpleImputer(strategy='mean')
     X_input = [[cloud_coverage, visibility, temperature, dew_point, relative_humidity, wind_speed, station_pressure]]
     X_input_imputed = imputer.fit_transform(X_input)
 
     # Make prediction
     prediction = solar_model.predict(X_input_imputed)[0]
+    return prediction
 
-    return render_template('solar_result.html', prediction=prediction)
 
-@app.route('/predict/wind/result', methods=['POST'])
-def predict_wind_result():
+#@app.route('/predict/wind/result', methods=['POST'])
+#def predict_wind_result():
     # Get input data from the form
     # Assume similar input fields as in the previous code for wind prediction
     # ...
 
     # Make prediction using the wind model
     # ...
-    if request.method == 'POST':
-        # Get the input values from the form
-        pressure = float(request.form['pressure'])
-        wind_direction = float(request.form['wind_direction'])
-        wind_speed = float(request.form['wind_speed'])
+ #   if request.method == 'POST':
+  #      # Get the input values from the form
+   #     pressure = float(request.form['pressure'])
+    #    wind_direction = float(request.form['wind_direction'])
+     #   wind_speed = float(request.form['wind_speed'])
 
         # Make a prediction using the trained model
-        prediction = wind_model.predict([[pressure, wind_direction, wind_speed]])
+      #  prediction = wind_model.predict([[pressure, wind_direction, wind_speed]])
+        #print(request.form)
 
-    return render_template('wind_result.html', prediction=prediction)
+
+    #return render_template('wind_result.html', prediction=prediction)
+def windresult(pressure,wind_direction,wind_speed):
+    prediction = wind_model.predict([[pressure, wind_direction, wind_speed]])
+    return prediction
+
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
